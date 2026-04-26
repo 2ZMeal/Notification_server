7cdf6f6dd22f0c34acf3c7f2eb19f5d5c89c4a3a
t2025-m0135 <junwon2631@gmail.com>
2026-04-26 17:57:01 +0900

    chore : notification spring project 설정

---
+++ src/test/java/com/ezmeal/notification/NotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezmeal.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
