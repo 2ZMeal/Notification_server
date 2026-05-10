665a79ef4256a8766bd3323df3dbaaf31820e659
t2025-m0135 <junwon2631@gmail.com>
2026-05-11 00:55:01 +0900

    feat : NotificationApplication 어노테이션 추가 for common module

--- src/main/java/com/ezmeal/notification/NotificationApplication.java
+++ src/main/java/com/ezmeal/notification/NotificationApplication.java
@@ -2,10 +2,16 @@ package com.ezmeal.notification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@EnableScheduling
 @EnableFeignClients
+@SpringBootApplication(scanBasePackages = {"com.ezmeal.notification", "com.ezmeal.common"})
+@EntityScan(basePackages = {"com.ezmeal.notification", "com.ezmeal.common"})
+@EnableJpaRepositories(basePackages = {"com.ezmeal.notification", "com.ezmeal.common"})
 public class NotificationApplication {
 
     public static void main(String[] args) {
